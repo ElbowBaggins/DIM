91996cf1663130e18770845ac3baf83f8f1c37c5
Elijah Woodward <elijah@elbow-baggins.net>
2020-06-11 19:09:52 -0500

    Update travis_build.sh

--- build/travis_build.sh
+++ build/travis_build.sh
@@ -3,6 +3,7 @@
 set -o pipefail
 
 find . -type d -name "node_modules" -prune -o -type f -iregex '.*.ts\|.*.js\|.*.tsx\|.*.jsx' -exec ./build/set-mtime-to-md5.sh {} \; && echo Set mtime to MD5 complete!
+rm -rf destiny-icons
 git clone https://github.com/justrealmilk/destiny-icons.git destiny-icons
 yarn test
 yarn run lint-check
